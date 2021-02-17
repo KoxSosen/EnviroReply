262300f526f63422b898a2f11148464877710d0b
KoxSosen <whyconfused2@gmail.com>
2021-02-17 08:34:07 +0100

    Add plugins & server icon.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-	const Discord = require('discord.js'); // libs
+const Discord = require('discord.js'); // libs
 	const bot = new Discord.Client();
 	const config = require("./config.json"); //config
-	var domain = ["setup domain"]
+	var domain = ["setup domain", "setup a domain", "set up a domain", "set up domain"]
 	var panel = ["log into the panel"]
 	var crash = ["server crashed", "server keeps crashing", "server crashing"]
 	var lagg = ["server is lagging", "server laggs", "server is still lagging"]
@@ -9,42 +9,46 @@
 	var flags = ["aikar flags", "aikars flags"]
 	var java = ["java 11", "java11"]
 	var timings = ["timings report"]
+	var SexyNerd = ["who is the sexiest nerd"]
+	var mostsexyest = ["sexiest nerd of all"]
+	var servericon = ["server icon"]
+	var plugins = ["install plugins"]
 
 
 		bot.on('message', (message) => {
 			if(message.author.bot) return;
 			const msgcontent = message.content.toLowerCase();
-			if (message.channel.id === '808324993422983203', '807739045974114324') {
-			for (var i=0; i < domain.length; i++) {	
+			if (message.channel.id === '732623827917602837' || message.channel.id === '732623846137528332') {
+			for (var i=0; i < domain.length; i++) {
 			if (msgcontent.includes(domain[i])) {
-		    message.channel.send({embed: {	
+		    message.channel.send({embed: {
 			title: "EnviroMC Reply",
 			url: "https://github.com/KoxSosen/EnviroReply",
 			color: 3066993,
-			description: "Please check the following guide: https://www.namecheap.com/support/knowledgebase/article.aspx/9765/2208/how-can-i-link-my-domain-name-to-a-minecraft-server/"	
+			description: "Please check the following guide: https://www.namecheap.com/support/knowledgebase/article.aspx/9765/2208/how-can-i-link-my-domain-name-to-a-minecraft-server/"
 			}});
 			}}
-							
+
 			for (var e=0; e < panel.length; e++) {
 			if (msgcontent.includes(panel[e])) {
 			message.channel.send({embed: {
-			title: "EnviroMC Reply", 
+			title: "EnviroMC Reply",
 			url: "https://github.com/KoxSosen/EnviroReply",
 			color: 3066993,
-			description: "Hello! To log into the panel, please visit https://panel.enviromc.com/, and enter your credentials. If you run into any issues, please open a ticket in <#732619830787702916>."	
+			description: "Hello! To log into the panel, please visit https://panel.enviromc.com/, and enter your credentials. If you run into any issues, please open a ticket in <#732619830787702916>."
 			}});
 			}}
-			
+
 			for (var b=0; b < crash.length; b++) {
 			if (msgcontent.includes(crash[b])) {
 			message.channel.send({embed: {
 			title: "EnviroMC Reply",
 			url: "https://github.com/KoxSosen/EnviroReply",
 			color: 3066993,
-			description: "Hello! If your server crashed, please upload the crash report into a paste service. The repor can be fund in home/continer/crash-reports/. After that, please open a ticket with your server id in <#732619830787702916>."	
+			description: "Hello! If your server crashed, please upload the crash report into a paste service. The report can be found in home/continer/crash-reports/. After uploading the reports, please open a ticket with your server id in <#732619830787702916>."
 			}});
 			}}
-			
+
 			for (var d=0; d < lagg.length; d++) {
 			if (msgcontent.includes(lagg[d])) {
 			message.channel.send({embed: {
@@ -52,15 +56,15 @@
 			url: "https://github.com/KoxSosen/EnviroReply",
 			color: 3066993,
 			description: "If your server is lagging, please consider sending a timings report in <#732618796401164360>."
-			}});	
+			}});
 			}}
-			
-			
+
+
 			for (var f=0; f < thank.length; f++) {
 			if (msgcontent.includes(thank[f])) {
 			message.channel.send("https://media1.tenor.com/images/15bafc0b414757acab81650a6ff21963/tenor.gif?itemid=11238673");
 			}}
-			
+
 			for (var j=0; j < flags.length; j++) {
 			if (msgcontent.includes(flags[j])) {
 			message.channel.send({embed: {
@@ -70,9 +74,9 @@
 			description: "We do support Aikars flags. Please open a ticket, and our staff team will set them up for you."
 			}});
 			}}
-			
-			
-			for (var k=0; k < flags.length; k++) {
+
+
+			for (var k=0; k < java.length; k++) {
 			if (msgcontent.includes(java[k])) {
 			message.channel.send({embed: {
 			title: "EnviroMC Reply",
@@ -81,9 +85,9 @@
 			description: "We do support Java 11. Please open a ticket, and our staff team will set it up for you."
 			}});
 			}}
-			
-			
-			for (var l=0; l < flags.length; l++) {
+
+
+			for (var l=0; l < timings.length; l++) {
 			if (msgcontent.includes(timings[l])) {
 			message.channel.send({embed: {
 			title: "EnviroMC Reply",
@@ -92,17 +96,54 @@
 			description: "You can make a timings report using the command `timings report`."
 			}});
 			}}
-			
-		
-			
-			
-			
+
+			for (var m=0; m < SexyNerd.length; m++) {
+			if (msgcontent.includes(SexyNerd[m])) {
+			message.channel.send({embed: {
+			title: "EnviroMC Reply",
+			url: "https://github.com/KoxSosen/EnviroReply",
+			color: 3066993,
+			description: "The General nerd is by far the most sexy nerd"
+			}});
+			}}
+
+			for (var y=0; y < mostsexyest.length; y++) {
+			if (msgcontent.includes(mostsexyest[y])) {
+            message.channel.send("https://media1.tenor.com/images/27e6a4a6de06644d0d449c242ca1996d/tenor.gif?itemid=8371338");
+			}}
+
+			for (var n=0; n < SexyNerd.length; n++) {
+			if (msgcontent.includes(servericon[n])) {
+			message.channel.send({embed: {
+			title: "EnviroMC Reply",
+			url: "https://github.com/KoxSosen/EnviroReply",
+			color: 3066993,
+			description: "Hello! To install a server icon for your server, please refer to the following guide: https://www.youtube.com/watch?v=XD_4taPylB4 "
+			}});
+			}}
+
+			for (var v=0; v < SexyNerd.length; v++) {
+			if (msgcontent.includes(plugins[v])) {
+			message.channel.send({embed: {
+			title: "EnviroMC Reply",
+			url: "https://github.com/KoxSosen/EnviroReply",
+			color: 3066993,
+			description: "Hello! To install plugins for your server, please click on the linK: https://bukkit.gamepedia.com/Installing_Plugins "
+			}});
+			}}
+
+
+
+
+
+
+
 	}
-	});        
+	});
 
 	bot.login(config.token);
 
 	bot.on("ready", () => {
 	  console.log(`Logged in as ${bot.user.tag}!`);
-	  bot.user.setActivity('EnviroMc Helper', { type: 'PLAYING' });
+	  bot.user.setActivity('Secretly hacking the EnviroMc site...', { type: 'PLAYING' });
 	});
